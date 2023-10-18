421f73fa315742bfe6ef0a724e4808c1e6b6ec71
jun447 <junaidmumtaz16@gmail.com>
2023-10-18 15:21:44 +0500

    Added Screens and Naviagtion

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 export default function App() {
   return (
     <View style={styles.container}>
