ecd989d9ebb7927f413f0c05bf8fc74a48f68c46
jun447 <junaidmumtaz16@gmail.com>
2023-10-18 16:26:37 +0500

    Added Home components

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,14 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
 import React from 'react'
 
-const Home = () => {
+import { NativeStackScreenProps } from "@react-navigation/native-stack"
+import {RootStackParamList} from "../App"
+
+// import components
+
+import ProductItem from "../components/ProductItem"
+import Separator from '../components/Separator'
+
+// importing data
+import {PRODUCTS_LIST} from "../contants"
+
+type HomeProps = NativeStackScreenProps<RootStackParamList, "Home">
+
+const Home = ({navigation}:HomeProps) => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container} >
+        <FlatList
+            data={PRODUCTS_LIST}
+            keyExtractor={(item)=>item.id}
+            renderItem={({item})=>(
+            <ProductItem 
+                product={item}
+                //onPress={()=>navigation.navigate('Details',{product:item})}
+            />
+            )}
+            ItemSeparatorComponent={()=><Separator/>}
+        />
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:'flex-start',
+        justifyContent:'center',
+        padding:10,
+        backgroundColor:'#fff'
+    }
+})
